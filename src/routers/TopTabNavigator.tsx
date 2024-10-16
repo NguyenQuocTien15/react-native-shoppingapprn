ae18235201cf81e670643ef72399f15b72d53dc3
HOI <leconghoica@gmail.com>
2024-10-16 23:02:55 +0700

    rename order navigator

--- src/routers/TopTabNavigator.tsx
+++ src/routers/TopTabNavigator.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs'
-import OrderHistory from './Order/OrderHistory';
-import OrderReview from './Order/OrderReview';
-import OrderShipping from './Order/OrderShipping';
-import OrderWaittingShipping from './Order/OrderWaittingShipping';
-import OrderWaittingForConfirmation from './Order/OrderWaittingForConfirmation';
+import OrderHistory from './Order/OrderHistoryNavigator';
+import OrderReview from './Order/OrderReviewNavigator';
+import OrderShipping from './Order/OrderShippingNavigator';
+import OrderWaittingShipping from './Order/OrderWaittingShippingNavigator';
+import OrderWaittingForConfirmation from './Order/OrderWaittingForConfirmationNavigator';
 
 const TopTabNavigator = () => {
   const Tab = createMaterialTopTabNavigator();
@@ -19,14 +19,14 @@ const TopTabNavigator = () => {
       }}
       sceneContainerStyle={{backgroundColor: 'white'}}>
       <Tab.Screen
-        name="Chờ xác nhận"
+        name="Waitting"
         component={OrderWaittingForConfirmation}></Tab.Screen>
       <Tab.Screen
-        name="Chờ vận chuyển"
+        name="WaittingShipping"
         component={OrderWaittingShipping}></Tab.Screen>
       <Tab.Screen name="OrderShipping" component={OrderShipping}></Tab.Screen>
       <Tab.Screen name="OrderReview" component={OrderReview}></Tab.Screen>
-      <Tab.Screen name="OrderHistory" component={OrderHistory}></Tab.Screen>
+      <Tab.Screen name="OrderHistoryScreen" component={OrderHistory}></Tab.Screen>
     </Tab.Navigator>
   );
 }
