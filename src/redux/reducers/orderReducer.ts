ce5d1f2df85720b0a889f7fe163ab3056dfcc337
HOI <leconghoica@gmail.com>
2024-10-22 20:10:07 +0700

    store + orderReduces

--- src/redux/reducers/orderReducer.ts
+++ src/redux/reducers/orderReducer.ts
@@ -1,61 +1,17 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import {createSlice} from '@reduxjs/toolkit';
-
-const handleSyncCardData = async (data: any[]) => {
-  await AsyncStorage.setItem('order', JSON.stringify(data));
+// Định nghĩa trạng thái ban đầu cho đơn hàng
+const initialState = {
+  orders: [],
 };
 
-export interface OrderItem {
-  color: string;
-  description: string;
-  id: string;
-  imageUrl: string;
-  price: number;
-  quantity: number;
-  size: string;
-  title: string;
-}
-
-const initialState: OrderItem[] = [];
-
-const orderSlicer = createSlice({
-  name: 'order',
-  initialState: {
-    orderData: initialState,
-  },
-  reducers: {
-    addOrder: (state, action) => {
-      const items: any = state.orderData;
-      const item = action.payload;
-      const index = items.findIndex((element: any) => element.id === item.id);
-
-      if (index !== -1) {
-        items[index].quantity = item.quantity;
-      } else {
-        items.push(item);
-      }
-
-      state.orderData = items;
-      handleSyncCardData(items);
-    },
-    // Hàm xóa một sản phẩm khỏi giỏ hàng
-    removeOrderItem: (state, action) => {
-      const itemId = action.payload; // ID của sản phẩm cần xóa
-      state.orderData = state.orderData.filter((item: any) => item.id !== itemId);
-
-      // Sau khi xóa, bạn cần đồng bộ dữ liệu giỏ hàng
-      handleSyncCardData(state.orderData);
-    },
-    syncLocalStorage: (state, action) => {
-      state.orderData = action.payload;
-    },
-  },
-});
-
-export const orderReducer = orderSlicer.reducer;
-export const {
-  addOrder,
-  syncLocalStorage,
-  removeOrderItem,
-} = orderSlicer.actions;
-export const orderSelector = (state: any) => state.orderReducer.orderData;
+// Định nghĩa reducer cho đơn hàng
+export const orderReducer = (state = initialState, action: { type: any; payload: any; }) => {
+  switch (action.type) {
+    case 'UPDATE_ORDER':
+      return {
+        ...state,
+        orders: [...state.orders, action.payload],
+      };
+    default:
+      return state;
+  }
+};
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./reducers/authReducer";
 import { cartReducer } from "./reducers/cartReducer";
-import {orderReducer} from './reducers/orderReducer';
-
+import { orderReducer } from "./reducers/orderReducer";
 
 const store = configureStore({
   reducer: {
     authReducer,
-    cartReducer,
-    orderReducer
+    cartReducer
   }
 });
 
