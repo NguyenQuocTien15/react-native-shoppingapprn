9d04b894fe6c481d8cf006e2f2fd722c993e4dde
nguyenquoctien15 <nguyenquoctien08797@gmail.com>
2024-10-31 16:55:49 +0700

    result-filter

--- src/screens/home/ResultScreen.tsx
+++ src/screens/home/ResultScreen.tsx
@@ -7,11 +7,24 @@ import {colors} from '../../constants/colors';
 import { globalStyles } from '../../styles/globalStyles';
 import firestore from '@react-native-firebase/firestore'
 const ResultScreen = ({navigation,route}:any) => {
-  const {filterValues} = route.params;
+  const {
+    filterValues,
+  }: {
+  filterValues : {
+    categories: string[];
+    price: {
+      low: number;
+      high: number;
+    };
+    sortby: string;
+    rate: number;
+  };
+  } = route.params;
+
   const [isLoading,setIsLoading]=useState(false);
   //ds tìm thấy được- lưu trong mảng
   const[products,setProducts]=useState<ProductModel[]>([]);
-
+console.log(filterValues)
   //hàm useEffect được gọi lại khi mà filterValue có sự thay đổi
   useEffect(() => {
     getProducts();
@@ -23,21 +36,26 @@ const ResultScreen = ({navigation,route}:any) => {
     try {
       const snap = await firestore()
       .collection('products')
-      //.where('categories','array-contains-any',filterValues.categories)
+      .where('categories','array-contains-any',filterValues.categories)
        .where('price', '>=',filterValues.price.low)
        .where('price', '<',filterValues.price.high)
+       .where('rate', '>=', filterValues.rate) // Lọc theo rating
       .get()
       if (!snap.empty) {
         const items:ProductModel[]=[]
         snap.forEach((item:any)=>{
-          item.push({
+          items.push({
             id: item.id,
             ...item.data()
           })
-        })
+        });
+        setProducts(items); // Đặt dữ liệu cho products
+      }else{
+        setProducts([]); // Trường hợp không có dữ liệu
       }
       setIsLoading(false);
     } catch (error) {
+      console.error(error);
       setIsLoading(false);
     }
   }
