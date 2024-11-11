27647038f00049965667680460ec37b8004caff1
HOI <leconghoica@gmail.com>
2024-11-11 08:06:30 +0700

    fix ref firebase

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,9 +1,13 @@
 import firestore from '@react-native-firebase/firestore';
 
-const productRef = firestore().collection('products');
-const categoriesRef = firestore().collection('categories');
-const userRef = firestore().collection('users');
-const orderRef = firestore().collection('orders')
-const cartRef = firestore().collection('carts');
 
-export {productRef, categoriesRef, userRef, orderRef, cartRef};
+
+
+export const db = firestore();
+export const productRef = db.collection('products');
+export const categoriesRef = db.collection('categories');
+export const userRef = db.collection('users');
+export const orderRef = db.collection('orders');
+export const orderDetailRef = db.collection('order_details');
+export const orderStatusRef = db.collection('orderStatus');
+export const cartRef = db.collection('carts');
